007a62a55ef89681aaa397d10b8eeee4148badca
ismayton <ian.mayton@gmail.com>
2021-01-13 16:21:54 -0600

    .event and .user style, render users for each event on DOMContentLoaded

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,15 +10,22 @@ function renderEvents(json) {
     const main = document.querySelector('main')
     json.map( eventJson => {
         let event = document.createElement('div')
-        
+        event.classList.add('event')
         event.innerHTML = `<h2>${eventJson.title}</h2>
         <p>Date: ${eventJson.date}</p>
         <p>Location: ${eventJson.location}</p>
         <p>Description: ${eventJson.description}</p>
         <p>Seats Remaining: ${eventJson.capacity - eventJson.users.length}</p>
         `
-
+        eventJson.users.map(user => {
+            let userDiv = document.createElement('li')
+            userDiv.classList.add('user')
+            userDiv.innerText = `Name: ${user.name}
+            Email: ${user.email}`
+            console.log(userDiv) 
+            event.appendChild(userDiv)
+        })
+        
         main.appendChild(event)
-        console.log(eventJson)
     })
 }
