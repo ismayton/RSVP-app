eb55cf2cc80c79537a7f41ae79fa2e041483388d
ismayton <ian.mayton@gmail.com>
2021-01-14 18:35:17 -0600

    RSVP form added, sends fetch post request to users controller and adds event to user.events

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    def create
+        puts "lookie here"
+        puts params
+        user = User.find_or_create_by(email: params[:email], name: params[:name])
+        event = Event.find(params[:event_id])
+        if event
+            puts "event ID detected"
+            user.events << event
+            render json: user
+        else
+            render json: user
+        end   
+    end
 end
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,77 @@
+const BASE_URL = "http://localhost:3000"
+const EVENTS_URL = `${BASE_URL}/events`
+const USERS_URL = `${BASE_URL}/users`
+
 document.addEventListener('DOMContentLoaded', fetchEvents() )
 
 function fetchEvents() {
-    return fetch("http://localhost:3000/events")
+    return fetch(EVENTS_URL)
     .then(function(response) { return response.json(); })
     .then(function(json){ renderEvents(json) })
 }
 
 function renderEvents(json) {
     const main = document.querySelector('main')
-    json.map( eventJson => {
-        let event = document.createElement('div')
-        event.classList.add('event')
-        event.innerHTML = `<h2>${eventJson.title}</h2>
-        <p>Date: ${eventJson.date}</p>
-        <p>Location: ${eventJson.location}</p>
-        <p>Description: ${eventJson.description}</p>
-        <p>Seats Remaining: ${eventJson.capacity - eventJson.users.length}</p>
-        `
-        eventJson.users.map(user => {
-            let userDiv = document.createElement('li')
-            userDiv.classList.add('user')
-            userDiv.innerText = `Name: ${user.name}
-            Email: ${user.email}`
-            console.log(userDiv) 
-            event.appendChild(userDiv)
-        })
-        
-        main.appendChild(event)
-    })
+    json.map( eventJson => main.appendChild(eventHTML(eventJson)) )
 }
+
+// forms each event from single event JSON object //
+
+function eventHTML(eventJson) {
+    let event = document.createElement('div')
+    event.classList.add('event')
+    event.innerHTML = `<h2>${eventJson.title}</h2>
+    <p>Date: ${eventJson.date}</p>
+    <p>Location: ${eventJson.location}</p>
+    <p>Description: ${eventJson.description}</p>
+    <p>Seats Remaining: ${eventJson.capacity - eventJson.users.length}</p>
+    `
+    eventJson.users.map( user => event.appendChild(usersHTML(user)) )
+    event.appendChild(eventFormHTML(eventJson))
+    return event
+}
+
+// forms user class DOM objects from event.users //
+
+function usersHTML(user) {
+    let userDiv = document.createElement('li')
+    userDiv.classList.add('user')
+    userDiv.innerText = `Name: ${user.name}
+    Email: ${user.email}`
+    return userDiv
+}
+
+function eventFormHTML(eventJson) {
+    let form = document.createElement('form')
+    form.classList.add('user-form')
+    form.id = eventJson.id
+    form.innerHTML = `<h4>RSVP for this Event</h4>
+        <label>Name: </label>
+        <input type="text" name="name"></br>
+        <label>Email: </label>
+        <input type="email" name="email"></br>
+        <input type="submit" value="submit">
+        </form>`
+
+    form.addEventListener( "submit", function(event) {
+        event.preventDefault();
+        let formData = new FormData(this);
+        formData.append("event_id", form.id)
+        console.log(...formData)
+        addUser(formData);
+    })
+    return form
+}
+
+function addUser(formData) {
+    let configObj = {
+        method: 'post',
+        body: formData
+    }
+    let user = fetch(USERS_URL, configObj)
+    .then(function(response) { return response.json(); })
+    .then(function(json){ console.log(json) })
+
+    console.log(user)
+}
+ 
